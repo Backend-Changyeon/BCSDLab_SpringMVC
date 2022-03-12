44ff82c73665f7bfaa0198b7c9bdf79c794bee1b
ChangyeonYoo <devwgod@gmail.com>
2022-03-12 14:05:15 +0900

    jdbc setting

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,12 +1,20 @@
 package controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import service.LectureService;
+
+import javax.annotation.Resource;
+import java.sql.SQLException;
 
 @Controller
 public class HelloController {
+
+    @Resource(name = "lectureServiceImpl")
+    LectureService lectureService;
 
     @RequestMapping(value = "/helloBody", method = RequestMethod.GET)
     @ResponseBody
@@ -15,7 +23,8 @@ public class HelloController {
     }
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String helloBody(){
+    public String helloBody() throws SQLException, ClassNotFoundException {
+        lectureService.getLectures();
         return "hello";
     }
 }
